449044bbc94c4ccd1ab364bb0035ffd189dcfe79
Laurent Bosteels <laurent.bosteels@gmail.com>
2021-05-10 12:22:12 +0200

    added input & search button+ function

--- js/script.js
+++ js/script.js
@@ -1,32 +1,47 @@
 // API URL - 470f22cc8a2f35fbc5b2273fa0d779e6
-// Added &units=metric because the temp needs to be converted to °C
-//
+// Added &units=metric to the url because the temp needs to be converted to °C
 
 // Wrote a function so you can search on cityname (in the console)
 // Used string interpolation 
 
-function GetForecast (cityName) {
+function GetForecast(cityName) {
 
-const url = `http://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=470f22cc8a2f35fbc5b2273fa0d779e6&units=metric`;
+  const url = `http://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=470f22cc8a2f35fbc5b2273fa0d779e6&units=metric`;
 
-// Fetching the Forecast for one particular city, in this case London
-// const url = 'http://api.openweathermap.org/data/2.5/forecast?q={London}&appid=470f22cc8a2f35fbc5b2273fa0d779e6&units=metric';
+  // Fetching the Forecast for one particular city, in this case London
+  // const url = 'http://api.openweathermap.org/data/2.5/forecast?q={London}&appid=470f22cc8a2f35fbc5b2273fa0d779e6&units=metric';
 
-// Make a request for a user with a given ID
-axios.get(url)
+  // Make a request for a user with a given ID
+  axios.get(url)
 
-  .then(function (response) {
-    // handle success
-    // console.log(response.data.list[0].main.temp);
+    .then(function (response) {
+      // handle success
+      // console.log(response.data.list[0].main.temp);
 
-    // Showing the temp on the screen
-    document.getElementById('target').innerHTML = `It is now ${response.data.list[0].main.temp}°C in ${cityName}`;
+      // Showing the temp on the screen
+      document.getElementById('target').innerHTML = `It is now ${response.data.list[0].main.temp}°C in ${cityName}`;
 
-  })
+    })
 
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
+    .catch(function (error) {
+      // handle error
+      document.getElementById('target').innerHTML = error;
+      console.log(error);
+    })
+
+}
+
+// 
+
+window.onload = function () {
+  document.getElementById('sendButton').onclick = function () {
+
+    // Getting the city from the input
+    const cityName = document.getElementById('cityTextInput').value;
+
+    // Calling the getForecast function
+    GetForecast(cityName);
+
+  }
 
 }
