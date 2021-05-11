0fbd4bd2cc529e5a1810d18fc6fede28a859d866
Laurent Bosteels <laurent.bosteels@gmail.com>
2021-05-11 09:10:57 +0200

    added days

--- js/script.js
+++ js/script.js
@@ -8,20 +8,49 @@ function GetForecast(cityName) {
 
   const url = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=d8d527c15354bfe5906040af032fc58a&units=metric`;
 
-  // Fetching the Forecast for one particular city, in this case London
-  // const url = 'http://api.openweathermap.org/data/2.5/forecast?q={London}&appid=d8d527c15354bfe5906040af032fc58a&units=metric';
-
   // Make a request for a user with a given ID
   axios.get(url)
-
     .then(function (response) {
-      // handle success
-      // console.log(response.data);
-      // console.log(response.data.list[0].main.temp);
+      // const dailyForecast = response.data.list;
+      // console.log(cityName, dailyForecast);
+      
+      // CREATING A FOR LOOP FOR A 5 DAY FORECAST
+      let container = document.getElementById('container');
+
+      for (let i = 0; i < response.data.list.length; i += 8) {
+
+        // CREATING THE HTML PART 1
+        let icon = response.data.list[i].weather[0].icon
+        let img = document.createElement('img')
+        let h1 = document.createElement("h1");
+        let h3 = document.createElement("h3");
+        let h4 = document.createElement("h4");
+        let div = document.createElement("div");
+        div.classList.add('one-day');
+
+        // REFORMATTING THE DATE TO A DAY WITH MOMENT.JS
+        const m = moment (response.data.list[i].dt_txt);
+        
+        // CREATING THE HTML PART 2
+        let displaytitle = document.createTextNode(response.data.list[i].weather[0].description);
+        let displayDescription = document.createTextNode(m.format('dddd'));
+        let displayPrice = document.createTextNode(Math.round(response.data.list[i].main.temp));
+
+        img.setAttribute('src', `http://openweathermap.org/img/wn/${icon}.png`)
+        h1.appendChild(displaytitle);
+        h3.appendChild(displayDescription);
+        h4.appendChild(displayPrice);
+
+        div.appendChild(img);
+        div.appendChild(h1);
+        div.appendChild(h3);
+        div.appendChild(h4);
+
+        document.body.appendChild(div);
+      }
 
       // Showing the temp of current day on the screen
-      document.getElementById('target').innerHTML = `It is now ${response.data.list[0].main.temp}°C in ${cityName}`;
-
+      // document.getElementById('target').innerHTML = `It is now ${response.data.list[0].main.temp}°C in ${cityName}`;
     })
 
     .catch(function (error) {
@@ -29,17 +58,15 @@ function GetForecast(cityName) {
       document.getElementById('target').innerHTML = ('Please make sure the city is spelled correctly.');
       console.log(error);
     })
-
 }
 
 window.onload = function () {
   document.getElementById('sendButton').onclick = function () {
-
     // Getting the city from the input
     const cityName = document.getElementById('cityTextInput').value;
-
     // Calling the getForecast function
     GetForecast(cityName);
+
 
   }
 
