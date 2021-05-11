3340c08f5f48e688afc606dbd7aaef811111771d
Laurent Bosteels <laurent.bosteels@gmail.com>
2021-05-11 17:45:12 +0200

    fresh start

--- README.md
+++ README.md
@@ -9,3 +9,8 @@
 * Added a function so you can search the city temp by name (only in console still) using string interpolation
 * Temperature shows up in the html
 * Added an error message if the city not found / spelled incorrectly
+
+
+<!-- // DATE, WELKE DAG IS DIT
+WELKE TEMPERATUUR
+DESCRIPTION -->
--- js/script.js
+++ js/script.js
@@ -4,70 +4,66 @@
 // Wrote a function so you can search on cityname (in the console)
 // Used string interpolation 
 
-function GetForecast(cityName) {
+/* function GetForecast(cityName) {
+ */
+const url = `https://api.openweathermap.org/data/2.5/forecast?q=London&appid=d8d527c15354bfe5906040af032fc58a&units=metric&exclude=current,minutely,hourly,alerts`;
 
-  const url = `https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=d8d527c15354bfe5906040af032fc58a&units=metric`;
+// Make a request for a user with a given ID
+axios.get(url)
+  .then(function (response) {
 
-  // Make a request for a user with a given ID
-  axios.get(url)
-    .then(function (response) {
-      // const dailyForecast = response.data.list;
-      // console.log(cityName, dailyForecast);
+    //console.log(response)
+
+    const today = new Date();
+    const day = 60 * 60 * 24 * 1000;
+
+    const dateBins = {};
+    const nBins = 6; // there can be reports for up to 6 distinct dates
+
+    for (let i = 0; i < nBins; i++) {
+
+      // set up a bin (empty array) for each date
       
-      // CREATING A FOR LOOP FOR A 5 DAY FORECAST
-      let container = document.getElementById('container');
+      const date = new Date(today.getTime() + i * day);
+      dateBins[date.getDate()] = [];
+    }
 
-      for (let i = 0; i < response.data.list.length; i += 8) {
-        
-        let icon = response.data.list[i].weather[0].icon
-        let img = document.createElement('img')
-        let h2 = document.createElement("h2");
-        let h3 = document.createElement("h3");
-        let h4 = document.createElement("h4");
-        let divCard = document.createElement("div");
-        divCard.classList.add('col-sm-2', 'day');
+    const reports = response.data.list;
+    for (const report of reports) {
+      const reportDate = new Date(report.dt * 1000).getDate();
+      dateBins[reportDate].push(report);
+    }
 
-        // REFORMATTING THE DATE TO A DAY WITH MOMENT.JS
-        const m = moment (response.data.list[i].dt_txt);
-        
-        // CREATING THE HTML PART 2
-        let displayDescription = document.createTextNode(response.data.list[i].weather[0].description);
-        let displayDay = document.createTextNode(m.format('dddd'));
-        let displayTemp = document.createTextNode(Math.round(response.data.list[i].main.temp));
+    console.log(dateBins);
+    
+/* 
+     for (let i = 0; i < response.data.list.length; i += 8) {
+      let UnixTime = response.data.list[i].dt;
+      let myDate = new Date(UnixTime * 1000).toLocaleDateString("en", {
+        weekday: "long",
+      });
 
-        img.setAttribute('src', `http://openweathermap.org/img/wn/${icon}.png`)
-        h2.appendChild(displayDescription);
-        h3.appendChild(displayDay);
-        h4.appendChild(displayTemp);
+      document.write(myDate)
 
-        divCard.appendChild(img);
-        divCard.appendChild(h2);
-        divCard.appendChild(h3);
-        divCard.appendChild(h4);
+    } */
 
-        document.querySelector('.row').appendChild(divCard); 
 
-      }
+  })
 
-      // Showing the temp of current day on the screen
-      // document.getElementById('target').innerHTML = `It is now ${response.data.list[0].main.temp}°C in ${cityName}`;
-    })
+  .catch(function (error) {
+    // handle error
+    document.getElementById('target').innerHTML = ('Please make sure the city is spelled correctly.');
+    console.log(error);
+  })
 
-    .catch(function (error) {
-      // handle error
-      document.getElementById('target').innerHTML = ('Please make sure the city is spelled correctly.');
-      console.log(error);
-    })
-}
+/* } */
 
-window.onload = function () {
+/* window.onload = function () {
   document.getElementById('sendButton').onclick = function () {
     // Getting the city from the input
     const cityName = document.getElementById('cityTextInput').value;
     // Calling the getForecast function
     GetForecast(cityName);
-
-
   }
 
-}
+} */
